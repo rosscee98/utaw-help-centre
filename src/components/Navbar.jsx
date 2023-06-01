5d74cb1bb5fa1e1b4a07141aca8fbc8ada07aa90
Ross Clark <rosscee1998@gmail.com>
2023-06-01 11:42:33 +0100

    minor navbar accessibility changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,13 +5,13 @@ export default function Navbar() {
   return (
     <HStack borderBottom="1px solid lightgray" paddingBottom="10px">
       <LinkBox as={HStack} gap="5px">
-        <Image src="utaw-logo.webp" width="70px" />
+        <Image src="utaw-logo.webp" alt="UTAW" width="70px" height="70px" />
         <LinkOverlay
           as={ReactRouterLink}
           to="/"
           _hover={{ textDecoration: "underline tomato" }}
         >
-          <Heading size="lg" color="tomato">
+          <Heading size="lg" color="red">
             Help Centre
           </Heading>
         </LinkOverlay>
